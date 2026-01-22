b5749051f9bd82fcc41f71da26982dc972cd3cf3
YCF <yangch@chy-meter.com>
2026-01-22 17:16:45 +0800

    1.IO設定

--- SRC/main.h
+++ SRC/main.h
@@ -5,4 +5,49 @@ extern "C"
 {
 #endif
 
+#define	POW_KEY			PC2
+#define	DOWN_KEY			PC3
+#define	UP_KEY				PC4
+#define	CANCEL_KEY			PC5
+#define	CONFIRM_KEY		PF6
 
+#define	PWM0_BEEPER_SW		PB5
+
+#define	LED_H_SW			PA11
+#define	LED_M_SW			PA10
+#define	LED_L_SW			PA9
+
+#define	RSSI_1_PIN			PD0
+#define	RSSI_2_PIN			PD1
+#define	RSSI_3_PIN			PD2
+#define	RSSI_4_PIN			PD3
+
+#define	MP_PIN				PB4
+#define	KM_PIN				PB3
+#define	MS_PIN				PB2
+
+#define	DOT_PIN				PB8
+
+#define	N1_A_PIN				PA7
+#define	N1_B_PIN				PB1
+#define	N1_C_PIN				PB0
+#define	N1_D_PIN				PA8
+#define	N1_E_PIN				PC7
+#define	N1_F_PIN				PC6
+#define	N1_G_PIN				PA6
+
+#define	N2_A_PIN				PA13
+#define	N2_B_PIN				PA15
+#define	N2_C_PIN				PB12
+#define	N2_D_PIN				PB7
+#define	N2_E_PIN				PB11
+#define	N2_F_PIN				PA14
+#define	N2_G_PIN				PB13
+
+#define	N3_A_PIN				PA12
+#define	N3_B_PIN				PB15
+#define	N3_C_PIN				PB9
+#define	N3_D_PIN				PB6
+#define	N3_E_PIN				PB10
+#define	N3_F_PIN				PC14
+#define	N3_G_PIN				PB14
